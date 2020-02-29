1969f625b03aefce74bdd1b3d8ed44b0a549654a
Firdavs0007 <f.zikirovhulk@gmail.comgit conflig user.name Firdavs0007>
2020-02-29 23:33:10 +0500

    Add existing file

---
+++ webpage/music_ex1.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Music Viewer</title>
+		
+		<link href="viewer.css" type="text/css" rel="stylesheet">
+	</head>
+
+	<body>
+		<div id="header">
+			<h1>190M Music Playlist Viewer</h1>
+			<h2>Search Through Your Playlists and Music</h2>
+		</div>
+		
+		
+		<div id="listarea">
+			<ul id="musiclist">
+
+				
+					<li class="mp3item">
+						<a href="https://courses.cs.washington.edu/courses/cse190m/09sp/labs/3-music/solution/songs/190M%20Rap.mp3">
+							190M Rap.mp3						</a>
+					</li>
+
+					
+					<li class="mp3item">
+						<a href="https://courses.cs.washington.edu/courses/cse190m/09sp/labs/3-music/solution/songs/Be%20More.mp3">
+							Be More.mp3						</a>
+					</li>
+
+					
+					<li class="mp3item">
+						<a href="https://courses.cs.washington.edu/courses/cse190m/09sp/labs/3-music/solution/songs/Drift%20Away.mp3">
+							Drift Away.mp3						</a>
+					</li>
+
+					
+					<li class="mp3item">
+						<a href="https://courses.cs.washington.edu/courses/cse190m/09sp/labs/3-music/solution/songs/Hello.mp3">
+							Hello.mp3						</a>
+					</li>
+
+					
+					<li class="mp3item">
+						<a href="https://courses.cs.washington.edu/courses/cse190m/09sp/labs/3-music/solution/songs/Just%20Because.mp3">
+							Just Because.mp3						</a>
+					</li>
+
+					
+					<li class="mp3item">
+						<a href="https://courses.cs.washington.edu/courses/cse190m/09sp/labs/3-music/solution/songs/Panda%20Sneeze.mp3">
+							Panda Sneeze.mp3						</a>
+					</li>
+
+								</ul>
+		</div>
+	
+</body></html>
